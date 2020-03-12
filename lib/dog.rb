3990703245dda419bb728bc33620eb29e860f18c
Angeleen MCDONNELL Wilson <angeleenmc@gmail.com>
2020-03-12 07:32:25 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -72,6 +72,6 @@ class Dog
   
   def update
     sql = "UPDATE dogs SET name = ?, breed = ?, WHERE id = ?"
-    DB[:conn].execute(sql, self.name, self.breed, self.id)
+    DB[:conn].execute(sql, @name, @breed, @id)
   end
 end
